30be4d4663b337eb9ba575c887e33664699a13c0
shehrozlakhani <128062231+shehrozlakhani@users.noreply.github.com>
2023-05-17 09:05:22 -0700

    Add files via upload

---
+++ CH1 TO CH10/app.js
@@ -0,0 +1,312 @@
+// #############################          CHAPTER 1                         ###########
+// Chapter 1 (Alerts)
+
+
+// alert("first name")
+// alert("last name")
+// alert("Email")
+// alert("Phone Number")
+// alert("Password")
+// alert("You are learning jawa script!")
+// alert("Hello from the other side")
+
+
+// #############################          CHAPTER 1 END                      ###########
+
+
+
+
+// #############################          CHAPTER 2                       ###########
+// Chapter 2 (Variables for string)
+
+// var camelCase = "Jawascript";
+
+// var js ;
+// js = "java script <br>";
+// document.write(js);
+
+// var teamName = alert("Team name");
+
+// var bestMan = "charlie";
+// bestMan = "new string <br> ";
+// document.write(bestMan)
+
+
+
+// #############################          CHAPTER 2 END                       ###########
+
+
+
+
+// #############################          CHAPTER 3                       ###########
+// Chapter 3 (Variables for numbers)
+
+// var caseQty ;
+
+// caseQty = 144;
+
+// var num = 9;
+
+// var one = 4 + 4;
+// var second = one;
+// document.write(second)
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// document.write(orderTotal) 
+//output is 110
+
+
+
+
+
+
+// #############################          CHAPTER 3 END                       ###########
+
+
+
+// #############################          CHAPTER 4                       ###########
+
+// Chapter 4 (Variable names Legal and Illegal)
+
+// var productcost = 3.54; 
+
+// var nameOfband ;
+
+// var legallyName = 5;
+
+// var firstLastname ;
+
+// // legal variable
+ 
+// var $hello;
+// var _hello;
+// var hello
+
+// // illegal variable
+
+// var 123;
+// var 456llo
+// var hello hello
+// var function;
+
+// #############################          CHAPTER 4 END                       ###########
+
+
+// #############################          CHAPTER 5                        ###########
+// Chapter 5 (Math Expression I)
+
+// name of operator is modulus and symbol of modulus is % 
+
+// var num = 20 % 6;
+// output is 2
+
+// the result of 1,000 multiplied by 2,000
+// output is 1,000,000
+
+// var sum = 33 % 5;
+// var remain = sum;
+
+// var $sum = 32 % 6;
+// alert($sum)
+
+
+// #############################          CHAPTER 5 END                        ###########
+// #############################          CHAPTER 6                         ###########
+
+// Math Expression II
+
+
+
+// var x = 5;
+// x++;
+// console.log(x);
+// ++x;
+// console.log(x);
+
+
+
+// var y = 100;
+// --y;
+// console.log(y);
+
+
+// var x = 50;
+// var y = x++;
+
+// console.log(y);
+
+
+
+// var y = 50;
+// var z = --y;
+
+// console.log(z);
+
+
+// var newNum = 10;
+// --newNum;
+
+// console.log(newNum);
+
+
+// var orignalValue = 10;
+// ++orignalValue;
+
+// console.log(orignalValue);
+
+
+// var a = 110;
+// a++;
+
+// alert(a);
+
+
+// #############################          CHAPTER 6 END                        ###########
+
+
+// #############################          CHAPTER 7    ###########
+// Math Expression III
+
+// var calculatedNum = 2 + 2 * 6;
+// console.log(calculatedNum);
+
+
+// var calculatedNum2 = (2 + 2) * 6;
+// console.log(calculatedNum2);
+
+
+// var calculatedNum3 = (2 + 2) * (4 + 2);
+// console.log(calculatedNum3);
+
+
+// var calculatedNum4 = (2 + 2) * 4 + 2;
+// console.log(calculatedNum4);
+
+
+// var calculatedNum5 = (2 + 2) * (4 + 10);
+// console.log(calculatedNum5);
+
+
+// var calculatedNum6 = 2 + 2 * 4 + 10;
+// console.log(calculatedNum6);
+
+
+// var calculatedNum7 = 4 / (2 * 4);
+// console.log(calculatedNum7);
+
+
+// #############################          CHAPTER 7  END                  ###########
+
+
+// #############################          CHAPTER 8                  ###########
+// // Concatenating Text Strings
+
+
+// var num = "2" + "2";
+// console.log(num);
+
+
+// var message = "Hello," + "Dolly";
+// alert(message);
+
+// alert("33" + 3);
+
+
+// var combine = "Pakistan " + "Zindabad";
+// alert(combine);
+
+
+// var num1 = 123;
+// var alphabet = "abc";
+// var combination = num1 + alphabet;
+// console.log(combination);
+
+// var str1 = "This is going to be ";
+// var str2 = "a beautiful Journey";
+// var combStr = str1 + str2;
+
+// console.log(combStr);
+
+// #############################   \\\       CHAPTER 8 END                  ###########
+
+
+
+// #############################   \\\       CHAPTER 9     ###########
+
+// Prompt
+
+
+// var firstName = prompt("Enter first name");
+// console.log(firstName)
+
+
+// var country = prompt("Country?", "China");
+// console.log(country)
+
+
+// var yourName = prompt("Enter Your Name");
+// console.log(yourName)
+
+
+// var age = prompt("Enter Your age", "18");
+// console.log(age)
+
+
+// var message = "What is Your Favorite Subject";
+// var defaultResponse = "Maths";
+
+// var input = prompt (message , defaultResponse);
+
+// console.log(input);
+
+
+// var color = prompt
+
+
+
+// #############################   \\\       CHAPTER 9                   ###########
+
+
+// #############################   \\\       CHAPTER 10                   ###########
+// if Statement
+
+
+// var city = "Karachi";
+// if ((city = "Karachi")) {
+//   console.log("The City OF Lights");
+// }
+
+
+// x = 1;
+// y = 1;
+// if (x === y) {
+//   var z = prompt("Find The Value of z");
+// }
+
+// var Zipcode = prompt("Enter Your ZipCode", "10010");
+// if (Zipcode == 10010) {
+//   alert("Karachi");
+// } else {
+//   alert("Please write correct city");
+// }
+
+
+// var x = 1;
+// if (x == 1) {
+//   x = 2;
+// }
+// console.log(x);
+
+
+
+// #############################   \\\       CHAPTER 10 END ###########
+
+
+
+
+
+
+
+
